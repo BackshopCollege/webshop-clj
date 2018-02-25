137e746c1defedde03cf048a12ea4c2d94ea3cc7
Jivago Alves <jivagoalves@gmail.com>
2018-02-25 00:12:14 +0000

    Add handler for `GET /url/:url` (GREEN)

--- src/webshop/core.clj
+++ src/webshop/core.clj
@@ -1,9 +1,9 @@
 (ns webshop.core
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
-            [ring.util.response :refer [created]]
+            [ring.util.response :refer [created redirect]]
             [cheshire.core :as json]
-            [compojure.core :refer [routes POST]]
+            [compojure.core :refer [routes POST GET]]
             [webshop.db :as db]
             ))
 
@@ -24,11 +24,17 @@
     (db/save db resource)
     (created "" (json/encode resource))))
 
+(defn- lookup-url
+  [{:keys [db]} url]
+  (redirect (db/lookup db url)))
+
 (defn new-handler
   [app]
   (routes
     (POST "/url" {body :body}
-          (shorten-url app (:url (parse-body body))))))
+          (shorten-url app (:url (parse-body body))))
+    (GET "/url/:url" {{url :url} :params}
+         (lookup-url app url))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; webserver entrypoint
@@ -39,7 +45,10 @@
     {:shorten (constantly "short")
      :db (reify db/IDb
            (save [_ resource]
-             (println (str "Saved: " resource))))}))
+             (println (str "Saved: " resource)))
+           (lookup [_ resource]
+             (println (str "Found: " resource))
+             "http://example.com"))}))
 
 (defn -main [& args]
   ; #' means "use the var"
